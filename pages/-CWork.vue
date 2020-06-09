17435e9418b15b9feeb9434e746affe253d3be63
manak1 <mana23happy@hotmail.co.jp>
2020-06-09 23:20:53 +0900

    chore: add page in animation

--- pages/-CWork.vue
+++ pages/-CWork.vue
@@ -4,14 +4,23 @@
       <div class="flex-grow"></div>
       <div class="c-work__head">
         <h2>WORK</h2>
+        <p>{{ index }}</p>
         <div class="c-slider">
           <div class="c-slider__item justify-center flex items-center">
             <div class="c-desc w-2/5">
-              <h3 class="c-desc__title">{{ getWork.title }}</h3>
-              <span class="c-desc__category">{{ getWork.tag }}</span>
-              <p class="c-desc__text">
-                {{ getWork.text }}
-              </p>
+              <div class="overflow-hidden">
+                <h3 class="c-desc__title">{{ getWork.title }}</h3>
+              </div>
+              <div class="overflow-hidden">
+                <span class="c-desc__category"
+                  ><span class="c-desc__categoryBar" />{{ getWork.tag }}</span
+                >
+              </div>
+              <div class="overflow-hidden">
+                <p class="c-desc__text">
+                  {{ getWork.text }}
+                </p>
+              </div>
             </div>
             <div class="c-item w-3/5">
               <a :href="getWork.url" target="_blank">
@@ -24,26 +33,33 @@
             </div>
           </div>
           <div class="c-slider__left">
-            <a href="#" class="c-slider__cursor">
+            <a
+              class="c-slider__cursor block"
+              @click.prevent="previousProject()"
+            >
               <img
                 class="c-slider__icon"
                 src="@/assets/images/work/arrow_left.svg"
-                alt=""
+                alt="一つ戻る"
               />
             </a>
           </div>
           <div class="c-slider__right">
-            <a href="" class="c-slider__cursor">
+            <a href="#" class="c-slider__cursor">
               <img
                 class="c-slider__icon"
                 src="@/assets/images/work/arrow_right.svg"
-                alt=""
+                alt="一つ進む"
+                @click.prevent="nextProject()"
               />
             </a>
           </div>
         </div>
         <div class="c-buttons mt-24">
-          <a href="" class="c-buttons__buttonm border px-8 py-2 inline-block">
+          <a
+            class="c-buttons__buttonm border px-8 py-2 inline-block"
+            @click.prevent="animation_in()"
+          >
             see more
           </a>
         </div>
@@ -54,6 +70,7 @@
 </template>
 
 <script>
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin'
 import { works } from '@/static/api/works.json'
 export default {
   data() {
@@ -66,11 +83,52 @@ export default {
     getWork() {
       return this.workData[this.index]
     }
+  },
+  methods: {
+    nextProject() {
+      if (this.index + 1 >= this.workData.length) {
+        this.index = 0
+      } else {
+        this.index = this.index + 1
+      }
+    },
+    previousProject() {
+      if (this.index - 1 < 0) {
+        this.index = this.workData.length - 1
+      } else {
+        this.index = this.index - 1
+      }
+    },
+    /* 
+    横から白いボックスが出てきて表示する
+    消える時も同じく白いボックス消えて↑
+    */
+    animation_in() {
+      // black covers
+      const titleHide = CSSRulePlugin.getRule('.c-desc__title:after')
+      const textHide = CSSRulePlugin.getRule('.c-desc__text:after')
+      const tagHide = CSSRulePlugin.getRule('.c-desc__category:after')
+      // white covers
+      const titleHide2 = CSSRulePlugin.getRule('.c-desc__title:before')
+      const textHide2 = CSSRulePlugin.getRule('.c-desc__text:before')
+      const tagHide2 = CSSRulePlugin.getRule('.c-desc__category:before')
+
+      const tl = this.$gsap.timeline({
+        repeat: 0
+      })
+      tl.to(titleHide, 0.8, { width: '0%', ease: 'Power3.easeInOut' }, 1)
+      tl.to(textHide, 0.8, { width: '0%', ease: 'Power3.easeInOut' }, 1)
+      tl.to(tagHide, 0.8, { width: '0%', ease: 'Power3.easeInOut' }, 1)
+
+      tl.to(titleHide2, 0.6, { width: '0%', ease: 'Power1.easeInOut' }, 2)
+      tl.to(textHide2, 0.6, { width: '0%', ease: 'Power1.easeInOut' }, 2)
+      tl.to(tagHide2, 0.6, { width: '0%', ease: 'Power1.easeInOut' }, 2)
+    }
   }
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .c-work {
   background-color: #191919;
 }
@@ -96,7 +154,16 @@ export default {
     transform: translateY(-50%);
   }
 
+  &__icon {
+    width: 30px;
+    height: 70px;
+    margin: 0 auto;
+    transition: 0.2s;
+    transition-timing-function: ease-out;
+  }
+
   &__cursor {
+    position: relative;
     display: inline-block;
     border: 1px solid #fff;
     border-radius: 50%;
@@ -106,15 +173,34 @@ export default {
     line-height: 70px;
     font-size: 40px;
 
-    &:hover {
-      background-color: #fff;
+    &:nth-child(1) {
+      &::after {
+        position: absolute;
+        top: 50%;
+        opacity: 0;
+        content: 'before';
+        height: 2px;
+        display: inline-block;
+      }
     }
-  }
 
-  &__icon {
-    width: 30px;
-    height: 70px;
-    margin: 0 auto;
+    &:hover {
+      position: relative;
+
+      .c-slider__icon {
+        transform: translateX(-60px);
+        transition: 0.2s;
+        transition-timing-function: ease-out;
+      }
+
+      &::after {
+        left: -30px;
+        animation: arrowBar 0.7s 1;
+        animation-fill-mode: forwards;
+        opacity: 1;
+        animation-timing-function: ease-in-out;
+      }
+    }
   }
 }
 
@@ -131,6 +217,12 @@ export default {
     transition-timing-function: cubic-bezier(0.215, 0.61, 0.355, 1),
       cubic-bezier(0.215, 0.61, 0.355, 1);
   }
+
+  &__img {
+    height: 328px;
+    width: 520px;
+    object-fit: contain;
+  }
 }
 
 .c-desc {
@@ -138,29 +230,117 @@ export default {
   text-align: left;
   max-width: 320px;
   &__title {
+    position: relative;
     font-size: 24px;
     font-weight: bold;
+
+    &:after {
+      position: absolute;
+      right: 0;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 30px;
+      background-color: #191919;
+      display: inline-block;
+      z-index: 999;
+    }
+
+    &:before {
+      position: absolute;
+      right: 0;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 30px;
+      background-color: #fff;
+      display: inline-block;
+      z-index: 999;
+    }
   }
 
   &__text {
+    position: relative;
     margin-top: 12px;
     font-size: 16px;
     line-height: 2;
+
+    &:after {
+      position: absolute;
+      right: 0px;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 100%;
+      background-color: #191919;
+      display: inline-block;
+      z-index: 999;
+    }
+
+    &:before {
+      position: absolute;
+      right: 0;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 100%;
+      background-color: #fff;
+      display: inline-block;
+      z-index: 999;
+    }
   }
 
   &__category {
     position: relative;
     display: inline-block;
     margin: 12px;
+
     &:before {
-      content: '';
-      width: 4px;
-      height: 20px;
-      background-color: #fff;
       position: absolute;
-      left: -12px;
-      top: 2px;
+      right: 0;
+      top: 0;
+      content: '';
+      width: 100%;
+      height: 30px;
+      background-color: #fff;
+      display: inline-block;
+      z-index: 999;
     }
+
+    &:after {
+      position: absolute;
+      right: -10px;
+      top: 0;
+      content: '';
+      width: 140%;
+      height: 30px;
+      background-color: #191919;
+      display: inline-block;
+      z-index: 999;
+    }
+  }
+
+  &__categoryBar {
+    width: 4px;
+    height: 20px;
+    background-color: #fff;
+    position: absolute;
+    left: -12px;
+    top: 2px;
+  }
+}
+
+@keyframes arrowBar {
+  0% {
+    width: 0px;
+  }
+
+  50% {
+    width: 120px;
+  }
+
+  100% {
+    width: 200px;
   }
 }
 </style>
